f1947b4d615588d0144eeed75eacf58b9cea3873
dzw <hu.duanzhiwen@aisino.com>
2022-05-10 17:33:47 +0800

    update

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
-import (
-	"Algorithm/graph"
-)
+import "fmt"
 
 func main() {
-	p1 := &graph.Point{Name: "A"}
-	p2 := &graph.Point{Name: "B"}
-	p3 := &graph.Point{Name: "C"}
-	p4 := &graph.Point{Name: "D"}
-	m := graph.NewMap(p1, p2, p3, p4)
-	m.InsertEdge(p1, p3)
-	m.InsertEdge(p1, p2)
-	m.InsertEdge(p2, p4)
+	nums := []int{1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4}
+	fmt.Println(maxSubArray(nums))
+}
 
-	m.InsertEdge(p3, p4)
-	m.InsertEdge(p1, p2)
-	m.InsertEdge(p1, p4)
-	m.Dfs(p1)
+func maxSubArray(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		if nums[0] > 0 && nums[1] > 0 {
+			return nums[0] + nums[1]
+		}
+		if nums[0] > nums[1] {
+			return nums[0]
+		} else {
+			return nums[1]
+		}
+	}
+	var sum int
+	for i := 0; i < len(nums); i++ {
+		if sum+nums[i] < sum {
+			s := maxSubArray(nums[i+1:])
+			if sum == 0 {
+				return s
+			}
+
+			// 尝试继续累加
+			if s > sum {
+				return s
+			} else {
+				return sum
+			}
+		}
+		//if sum+nums[i] < sum {
+		//	if i+1 < len(nums) {
+		//		if sum+nums[i]+nums[i+1] < sum {
+		//			s := maxSubArray(nums[i+1:])
+		//			if sum == 0 {
+		//				return s
+		//			}
+		//			if s > sum {
+		//				return s
+		//			} else {
+		//				return sum
+		//			}
+		//		}
+		//	}
+		//}
+		sum += nums[i]
+	}
+	return sum
 }
